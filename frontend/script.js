dfa3508a7e4806445c585b4095c94cb40dcc376b
s1xte3n <ntnhlapo16@gmail.com>
2025-06-12 00:20:04 +0200

    Git Actions Debug

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const counterElement = document.getElementById("visitor-count");
+    const apiEndpoint = 'https://1qd8fxffce.execute-api.eu-north-1.amazonaws.com/Prod/visitor-count';
 
     try {
-        const response = await fetch("https://pyoeir4fr2.execute-api.eu-north-1.amazonaws.com/Prod/visitor-count");
+        const response = await fetch(apiEndpoint);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
